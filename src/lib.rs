2c2daf1fac4af10365f2d6ae9fb4dfdf1944dff7
carlitosdummy <carlos256800@gmail.com>
2024-07-23 17:53:32 -0600

    simplify argument validation in Config::build

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,7 @@ pub struct Config {
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments :c");
-        }
+        if args.len() < 3 { return Err("not enough arguments"); }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
